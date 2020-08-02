3b05b895678efbf4247182d071ebe365213d11db
Leandro Moreira <30784301+leandromoreirati@users.noreply.github.com>
2020-08-02 10:34:28 -0300

    Fix RestAssured.baseURI

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -11,7 +11,7 @@ public class APITest {
 	
 	@BeforeClass
 	public static void setup() {
-		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+		RestAssured.baseURI = "http://tomcat:8000/tasks-backend";
 	}
 
 	@Test
